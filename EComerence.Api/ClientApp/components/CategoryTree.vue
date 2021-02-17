bc2d3461ad475135cdab9183cb56e3054159114f
michał kurecki <Michal.Kurecki@outlook.com>
2021-02-17 22:59:00 +0100

    FrontendWork

---
+++ EComerence.Api/ClientApp/components/CategoryTree.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-treeview
+    activatable
+    :categories="categories"
+  ></v-treeview>
+</template>
+<script>
+  export default {
+    data: () => {
+      return{
+        categories:[]
+      }
+    },
+    async fetch() {
+      this.categories = await fetch('https://localhost:44367/Categories').then((res) => res.json())
+    }
+  }
+</script>
---
+++ EComerence.Api/ClientApp/components/Product.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4">
+          {{product.name}}
+        </div>
+        <v-list-item-title class="headline mb-1">
+          {{product.producerName}}
+        </v-list-item-title>
+        <v-list-item-subtitle>{{product.description}}</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn
+        outlined
+        rounded
+        text
+      >
+        {{product.price}}
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    product:{
+      type: Object,
+      default: () => {}
+
+    }
+  }
+}
+</script>
