d0a1bfe2f650d369ae91c2f0b7ca423a59add0a2
michał kurecki <Michal.Kurecki@outlook.com>
2021-03-08 23:18:05 +0100

    slug

---
+++ EComerence.Api/ClientApp/pages/Product/_slug.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-hover>    
+    <template v-slot:default="{ hover }">
+      <v-card
+        :elevation="hover ? 24 : 6"
+        class="mx-auto my-12"
+        max-width="344"
+        outlined
+      >
+        <v-list-item three-line>
+          <v-list-item-content>
+            <div class="overline mb-4">
+              {{product.name}}
+            </div>
+            <v-list-item-title class="headline mb-1">
+              {{product.producerName}}
+            </v-list-item-title>
+            <v-list-item-subtitle>{{product.description}}</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-avatar
+            tile
+            size="80"
+            color="grey"
+          ></v-list-item-avatar>
+        </v-list-item>
+
+        <v-card-actions>
+          <v-btn
+            outlined
+            rounded
+            text
+          >
+            {{product.price}}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-hover>
+</template>
+<script>
+export default {
+   async asyncData({ params, $axios }) {
+            const product = await $axios.$get('https://localhost:44367/Products/'+params.slug)
+             return { product }
+   }
+}
+</script>
